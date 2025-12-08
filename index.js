d9ec6106fe3d13f5661ae0537216cb34d8512079
jeremytenjo <tenjojeremy@gmail.com>
2025-12-08 14:50:35 -0500

    chore: remove unused imports and variables from index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import { existsSync, lstatSync } from 'fs'
 import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 const plugin = {
   meta: {
