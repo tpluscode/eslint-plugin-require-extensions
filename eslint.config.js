9fad260d9a6685d0a7930ae4d16794c2ab4547a4
jeremytenjo <tenjojeremy@gmail.com>
2025-12-08 14:42:04 -0500

    feat: add ESLint configuration files for require-js-extension plugin

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import plugin from './index.js'
+
+export default [
+  {
+    files: ['**/*.js'],
+    plugins: {
+      'require-js-extension': plugin,
+    },
+    rules: {
+      'require-js-extension/require-js-extension': 'error',
+      'require-js-extension/require-index': 'error',
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'test.js'],
+  },
+]
